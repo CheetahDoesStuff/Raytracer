dfb7047eb339a2d1373692d9068c4f38b0c461bb
CheetahDoesStuff <bravestcheetah@gmail.com>
2026-05-27 21:26:52 +0200

    better noise

--- src/noise.rs
+++ src/noise.rs
@@ -1,10 +1,10 @@
 use nalgebra::Vector3;
 use rand::{rngs::SmallRng, seq::SliceRandom};
 
-use crate::utils::random_f32;
+use crate::utils::random_unit_vec;
 
 pub struct Perlin {
-    randfloat: [f32; 256],
+    randvec: [Vector3<f32>; 256],
     perm_x: [usize; 256],
     perm_y: [usize; 256],
     perm_z: [usize; 256],
@@ -12,17 +12,17 @@ pub struct Perlin {
 
 impl Perlin {
     pub fn new() -> Self {
-        let mut randfloat = [0.0; 256];
+        let mut randvec = [Vector3::new(0.0, 0.0, 0.0); 256];
         let mut rng: SmallRng = rand::make_rng();
         for i in 0..256 {
-            randfloat[i] = random_f32(&mut rng, 0.0, 1.0)
+            randvec[i] = random_unit_vec(&mut rng)
         }
 
         let perm_x = Perlin::generate_perm();
         let perm_y = Perlin::generate_perm();
         let perm_z = Perlin::generate_perm();
 
-        Perlin { randfloat, perm_x, perm_y, perm_z }
+        Perlin { randvec, perm_x, perm_y, perm_z }
     }
 
     pub fn noise(&self, point: Vector3<f32>) -> f32 {
@@ -30,35 +30,36 @@ impl Perlin {
         let v = point.y - point.y.floor();
         let w = point.z - point.z.floor();
 
-        let u = u * u * (3.0 - 2.0 * u);
-        let v = v * v * (3.0 - 2.0 * v);
-        let w = w * w * (3.0 - 2.0 * w);
-
         let i = point.x.floor() as isize;
         let j = point.y.floor() as isize;
         let k = point.z.floor() as isize;
-        let mut c = [[[0.0_f32; 2]; 2]; 2];
+        let mut c = [[[Vector3::new(0.0, 0.0, 0.0); 2]; 2]; 2];
 
         for di in 0..2 { 
             for dj in 0..2 { 
                 for dk in 0..2 {
-                    c[di][dj][dk] = self.randfloat[
+                    c[di][dj][dk] = self.randvec[
                         self.perm_x[((i + di as isize) & 255) as usize]
                             ^ self.perm_y[((j + dj as isize) & 255) as usize]
                             ^ self.perm_z[((k + dk as isize) & 255) as usize]
         ]}}}
-        Perlin::trilinear_interpolation(c, u, v, w)
+        Perlin::perlin_interpolation(c, u, v, w)
     }
 
-    pub fn trilinear_interpolation(c: [[[f32; 2]; 2]; 2], u: f32, v: f32, w: f32) -> f32 {
-        let mut accum: f32 = 0.0;
+    pub fn perlin_interpolation(c: [[[Vector3<f32>; 2]; 2]; 2], u: f32, v: f32, w: f32) -> f32 {
+        let uu = u * u * (3.0 - 2.0 * u);
+        let vv = v * v * (3.0 - 2.0 * v);
+        let ww = w * w * (3.0 - 2.0 * w);
+
+        let mut accum = 0.0;
         for i in 0..2 {
             for j in 0..2 {
                 for k in 0..2 {
-                    accum += (i as f32 * u + (1 - i) as f32 * (1.0 - u))
-                            * (j as f32 * v + (1 - j) as f32 * (1.0 - v))
-                            * (k as f32 * w + (1 - k) as f32 * (1.0 - w))
-                            * c[i][j][k];
+                    let weight_v = Vector3::new(u - i as f32, v - j as f32, w - k as f32);
+                    accum += (i as f32 * uu + (1 - i) as f32 * (1.0 - uu))
+                            * (j as f32 * vv + (1 - j) as f32 * (1.0 - vv))
+                            * (k as f32 * ww + (1 - k) as f32 * (1.0 - ww))
+                            * c[i][j][k].dot(&weight_v);
                 }
             }
         }
--- src/surface/textures/noise.rs
+++ src/surface/textures/noise.rs
@@ -13,6 +13,6 @@ impl NoiseTexture {
 
 impl Texture for NoiseTexture {
     fn sample(&self, _u: f32, _v: f32, pos: Vector3<f32>) -> Color {
-        Color::new(1.0, 1.0, 1.0) * self.noise.noise(self.scale * pos)
+        Color::new(1.0, 1.0, 1.0) * 0.5 * (1.0 + self.noise.noise(self.scale * pos))
     }
 }
